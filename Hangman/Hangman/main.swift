3678d06f97468c3549f1c7e1b4f6cbe9dc51a0ce
Chelsi Christmas <chelsichristmas@Chelsis-MacBook-Pro.local>
2019-10-11 15:16:14 -0400

    Initial Push

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -7,6 +7,27 @@
 //
 
 import Foundation
+import UIKit
 
-print("Hello, World!")
+let randomWord = "apple"
+var hiddenWord = Array (repeating: "_", count: randomWord.count) //
 
+//OR
+// var hiddenWord : [Characters] Array (repeating: "_", count: randomWord.count)
+
+var enteredChar = "p" //readline()
+var indices = [Int]() // updtaing indices to a set it makes the run time faster
+
+var arr = ["a","p","p","l","e"]
+for (index,char) in arr.enumerated( ) {
+if enteredChar == char {
+indices.append(index) //go into my empty array add the number for the index
+    }
+}
+//(loop2) updates the hidden word
+for (index,_) in arr.enumerated( ) {
+if indices.contains(index) {
+arr[index] = enteredChar
+}
+}
+print((hiddenWord))
